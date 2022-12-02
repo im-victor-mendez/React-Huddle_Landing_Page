686f3ba832b69f83cd8a226eea5210ec7a6c9784
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-12-01 21:16:36 -0600

    Init front structure.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,35 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <div id='company'>
+        <img src="" alt="" />
+        <h1>Huddle</h1>
+      </div>
+
+      <article>
+        <img src="" alt="" />
+
+        <div id='text'>
+          <h1>Build The Community Your Fans Will Love</h1>
+          <p>Huddle re-imagines the way we build communities.
+            You have a voice, but so does your audience.
+            Create connections with your users as you engage in genuine discussion.
+          </p>
+          <button>Register</button>
+        </div>
+      </article>
+
+      <div id='social-medias'>
+        <a className='social-media' href="http://">
+          <i></i>
+        </a>
+        <a className='social-media' href="http://">
+          <i></i>
+        </a>
+        <a className='social-media' href="http://">
+          <i></i>
+        </a>
+      </div>
     </div>
   )
 }
